a52196cbae7d7235a544d238345770bb99ba253f
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-09-04 05:45:28 +0530

    Update api.js
    during deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+// Local pe chalate time localhost, render pe deploy hone ke baad backend URL
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 export const uploadFile = async (data) => {
     try {
